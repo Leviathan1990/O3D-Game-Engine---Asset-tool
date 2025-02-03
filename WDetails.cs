f81ea58667e76cb2cc0cbf39ed5ea2c3386e501d
Winters1990 <80176890+Leviathan1990@users.noreply.github.com>
2025-02-03 14:07:52 +0100

    Add files via upload

---
+++ WDetails.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WDetails
+{
+    public static class WordDetails
+    {
+        // A list to hold the keywords.
+        private static List<string> keywords = new List<string>
+        {
+            "M_Resources",
+            "M_SetGameSceneRect",
+            "STARTMP3",
+            "si_Map_Description",
+            "si_Map_Author",
+            "si_Map_Players",
+            "si_Map_Width",
+            "si_Map_Height",
+            "m_EditMode",
+            "m_Devmap",
+            "m_DevMapMode",
+            "M_SetStartPositionByMouse",
+            "M_SetStartPosition",
+            "M_DefineStartPositions",
+            "m_SetTimer",
+            "M_SetLayerPosition",
+            "M_SelectLayer",
+            "M_TeamMode",
+        };
+
+        /// <summary>
+        /// Returns the keyword if it exists, otherwise a default message.
+        /// </summary>
+        public static string GetDetailedInfo(string searchTerm)
+        {
+            if (keywords.Contains(searchTerm))
+            {
+                return searchTerm;
+            }
+            return "No info available...";
+        }
+
+        /// <summary>
+        /// Returns all the keywords.
+        /// </summary>
+        public static IEnumerable<string> GetAllKeywords()
+        {
+            return keywords;
+        }
+    }
+}
